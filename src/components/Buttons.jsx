14dd4a98a8f9c9f54b693bac75943435d5f89431
malhotra-parul <parulm.business@gmail.com>
2020-08-23 15:22:23 +0530

    styled primary, secondary and tertiary buttons

--- src/components/Buttons.jsx
+++ src/components/Buttons.jsx
@@ -1,13 +1,11 @@
 import styled from "styled-components";
 
 const primaryBlue="#2D41AD"; 
-const secondaryBlue = "#F4F5F7";
-const secondaryTextColor = "#000";
-const tertiaryBorder = "#746FC0";
+const secondaryBlue = "#ddebfd";
 
 const Button = styled.button`
        
-        margin: none;
+        outline: none;
         padding: 10px 30px;
         border-radius: 5px;
         font-size: 1rem;
@@ -19,20 +17,18 @@ const Button = styled.button`
 export const PrimaryButton = styled(Button)`
         background: ${primaryBlue};
         color: #F4F5F7;
-        border-color: ${primaryBlue};
+        border: 1px solid ${primaryBlue};
         filter: drop-shadow(0px 0px 2px ${primaryBlue} );
 `;
 
 export const SecondaryButton = styled(Button)`
-        background: ${secondaryBlue};
-        color: ${secondaryTextColor};
-        border-color: ${primaryBlue};
-        filter: drop-shadow(0px 0px 2px ${primaryBlue} );
+        background: none;
+        color: ${primaryBlue};
+        border: 1px solid ${primaryBlue};
 `;
 
 export const TertiaryButton = styled(Button)`
-        background: ${secondaryBlue};
-        color: ${secondaryTextColor};
-        border-color: ${tertiaryBorder};
-        filter: drop-shadow(0px 0px 2px ${tertiaryBorder} );
+        background: none;
+        color: ${primaryBlue};
+        border: none;
 `;
