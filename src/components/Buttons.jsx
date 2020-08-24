91fd9b0240310397843c5340830ff7ed50a21c20
malhotra-parul <parulm.business@gmail.com>
2020-08-24 11:55:30 +0530

    added hover, focus, active and disabled states on button

--- src/components/Buttons.jsx
+++ src/components/Buttons.jsx
@@ -1,7 +1,6 @@
 import styled from "styled-components";
 import { typeScale, lightTheme, darkTheme } from "../utils";
 
-const theme = darkTheme;
 
 const Button = styled.button`
        
@@ -12,23 +11,41 @@ const Button = styled.button`
         cursor: pointer;
         font-family: ${lightTheme.primaryFont};
         min-width: 100px;
+
+        &:hover, &:focus{
+                background: ${lightTheme.primaryColorHover};
+        }
+        &:active{
+                background: ${lightTheme.primaryColorActive}
+        }
+        &:disabled{
+                background: ${lightTheme.primaryColorDisabled};
+                color: ${lightTheme.textColorInverted};
+                cursor: not-allowed;
+        }
     `;
 
 export const PrimaryButton = styled(Button)`
-        background: ${theme === darkTheme ? darkTheme.primaryColor : lightTheme.primaryColor};
+        background: ${lightTheme.primaryColor};
         color: #F4F5F7;
-        border: 1px solid ${theme === darkTheme ? darkTheme.primaryColor : lightTheme.primaryColor};
+        border: 1px solid ${lightTheme.primaryColor};
         filter: drop-shadow(0px 0px 1px ${lightTheme.primaryColor} );
 `;
 
 export const SecondaryButton = styled(Button)`
         background: none;
-        color: ${theme === darkTheme ? darkTheme.primaryColor : lightTheme.primaryColor};
-        border: 1px solid ${theme === darkTheme ? darkTheme.primaryColor : lightTheme.primaryColor};
+        color: ${lightTheme.primaryColor};
+        border: 1px solid ${lightTheme.primaryColor};
+        &:hover, &:active, &:focus{
+                color: ${lightTheme.textColorInverted}
+        }
 `;
 
 export const TertiaryButton = styled(Button)`
         background: none;
-        color: ${theme === darkTheme ? darkTheme.primaryColor : lightTheme.primaryColor};
+        color: ${lightTheme.primaryColor};
         border: none;
+        &:hover, &:active, &:focus{
+                color: ${lightTheme.textColorInverted}
+        }
 `;
--- src/utils/themes.js
+++ src/utils/themes.js
@@ -5,6 +5,7 @@ export const lightTheme = {
     primaryColor: blue[300],
     primaryColorHover: blue[200],
     primaryColorActive: blue[100],
+    primaryColorDisabled: neutral[400],
     textColorOnPrimary: neutral[100],
     textColor: neutral[500],
     textColorInverted: neutral[100],
@@ -16,6 +17,7 @@ export const darkTheme = {
     primaryColorHover: neutral[400],
     primaryColorActive: neutral[300],
     textColorOnPrimary: neutral[100],
+    primaryColorDisabled: neutral[600],
     textColor: neutral[100],
     textColorInverted: neutral[600],
     primaryFont
